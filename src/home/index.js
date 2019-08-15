7da57904cab9ef4ea7b48aa62edd5bd4b646e088
Carmen Chapa <cmchapa28@gmail.com>
2019-08-15 19:38:48 +0200

    Shape about in home

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../static/images/map.png'
+import {data as ProtasData} from '../data/generalTexts'
 
 export default class Home extends React.Component {
   render() {
@@ -8,12 +9,16 @@ export default class Home extends React.Component {
 				<div className="HomeBackground NabBarAvoiding">
 					<p className="HomeTitle">PROTAGONISTAS <br/>DEL PROGRESO</p>
 				</div>
-				<div className="FullHeight FlexStyle">
+				<div className="FullHeight FlexStyle" style={{flexDirection: 'column', flex: 1, alignSelf: 'center'}}>
 					<div style={secondContainerStyle}>
 						<Square text='¿Qué es Protagonistas del progreso?' color='#d8d5bd' size={'45vh'} fontSize={50}/>
 						<Rectangle src={require('../static/images/map.png')} width={'55vh'} height={'40vh'} />
 					</div>
+					
 				</div> 
+				<div className="FullHeight FlexStyle" style={thirdContainerStyle}>
+					<p>{ProtasData.introProtas}</p>
+				</div>
 			</div>
 			)
   }
@@ -24,11 +29,20 @@ const secondContainerStyle = {
 	flex: 1,
 	flexDirection: 'row',
 	justifyContent: 'space-around',
-	alignSelf: 'center',
+	alignItems: 'flex-end',
+	alignContent: 'center',
+	margin: 'auto',
 	marginLeft: '5%',
-	marginRight: '5%'
+	marginRight: '5%',
+	maxHeight: '45vh'
+}
+
+const thirdContainerStyle = {
+	marginLeft: '20vw',
+	marginRight: '20vw',
+	fontSize: '1.4rem'
 }
 
 const Square = (props) => <div className="Centred" style={{backgroundColor: props.color, width: props.size, height: props.size, padding: 40}}><p style={{fontSize: props.fontSize}}>{props.text}</p></div>
 
-const Rectangle = (props) => <div className="Centred" style={{backgroundColor: props.color, width: props.width, height: props.height, alignSelf: 'flex-end'}}><img src={props.src} width={'100%'} height={'100%'} alt="" /></div>
+const Rectangle = (props) => <div className="Centred" style={{backgroundColor: props.color, width: props.width, height: props.height, alignItems: 'flex-end'}}><img src={props.src} width={'100%'} height={'100%'} alt="" /></div>
