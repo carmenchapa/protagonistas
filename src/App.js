e73932f1dc4ed485c158f1bf32df7545c0292ed8
Carmen Chapa <cmchapa28@gmail.com>
2019-08-15 17:31:06 +0200

    Add routes and classes to main

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import Navigation from './navigation'
 import Home from './home'
 import Movimientos from './movimientos'
 import Protagonistas from './protagonistas'
+import Protagonista from './protagonistas/protagonista'
 import Ejes from './ejes'
 import Mapa from './mapa'
 
 function App() {
+	// console.log('App', this.context)
+	const {pathname} = window.location
   return (
-		  <Router>
-    <div>
-      <Navigation />
-      <div>
+		 <Router>
+    <div className="AppContainer">
+      <Navigation currentRoute={pathname}/>
+      <div className="AppContainer">
       <Route path={ROUTES.HOME} component={Home} />
       <Route path={ROUTES.MOVIMIENTOS} component={Movimientos} />
       <Route path={ROUTES.PROTAGONISTAS} component={Protagonistas} />
 			<Route path={ROUTES.EJES} component={Ejes} />
 			<Route path={ROUTES.MAPA} component={Mapa} />
+			{/* <Route path={ROUTES.MOVIMIENTO} component={Protagonista} /> */}
+			<Route path={ROUTES.PROTAGONISTA} component={Protagonista} />
 
       {/* <Route path="/proposals:id" component={DecisionPage} /> */}
       </div>
