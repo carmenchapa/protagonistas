3cfc7983d98d190882a2b8b130e9fc4a51d5f3b1
Carmen Chapa <cmchapa28@gmail.com>
2019-08-15 19:38:36 +0200

    Add text - external data

---
+++ src/data/movimientos.js
@@ -0,0 +1,7 @@
+export const protas = [
+	{
+		'nombre': 'comuna13',
+		'protagonists': 'comuna 13',
+		'texto': 'La comuna 13 ha sido reconocida internacionalmente como un referente de urbanismo social en pro de la transformación y la revitalización de la comunidad.  Se han gestado un sinnúmero de iniciativas que buscan evidenciar esas ganas de luchar contra la violencia que tanto los ha golpeado por años. La “Comuna 13 Graffitour” es uno de los ejemplos de transformación desde el arte. Se trata de un recorrido guiado por PERROGRAFF y su equipo, el cual busca analizar cómo a través de la música y el arte la comunidad se ha ido sanando e integrando, recibiendo día tras día a cientos de turistas y locales que admiran y se deleitan con el arte y sus historias.'
+	}
+]
---
+++ src/data/protagonistas.js
@@ -0,0 +1,22 @@
+export const protas = [
+	{
+		'nombre': 'perro',
+		'movimiento': 'comuna 13',
+		'texto': 'Texto '
+	},
+	{
+		'nombre': 'aka',
+		'movimiento': 'comuna 13',
+		'texto': 'Texto '
+	},
+	{
+		'nombre': 'martika',
+		'movimiento': 'comuna 13',
+		'texto': 'Texto '
+	},
+	{
+		'nombre': 'Doña Consuelo & Sebastián',
+		'movimiento': 'comuna 13',
+		'texto': 'Texto '
+	}
+]
