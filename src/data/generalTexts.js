5d00476aa40290f1e00a87a17cb65e04fe14bb10
Carmen Chapa <cmchapa28@gmail.com>
2019-08-16 05:59:31 +0200

    Export default general texts

--- src/data/generalTexts.js
+++ src/data/generalTexts.js
@@ -1,5 +1,8 @@
-export const data = {
+const data = {
 	'introProtas': `Protagonistas del Progreso es un movimiento transmedia con fines sociales, que a través de diferentes medios busca visibilizar la vida de personas en todo el mundo, comenzando por América Latina, que, con sus actos cotidianos logran transformar su realidad personal, de las personas que les rodean, y de su comunidad de forma extraordinaria. El movimiento pretende resaltar y darle importancia a las historias de vida de héroes anónimos - Protagonistas del Progreso - para que estos se conviertan en los modelos a seguir de las futuras generaciones.`,
 	'subTextProtas': "El movimiento de los Protagonistas del Progreso quiere visibilizar las historias de vida de héroes cotidianos, rompiendo con el paradigma de los modelos que inspiran actualmente.",
-	'thirdTextProtas': "Los Protagonistas del Progreso son héroes anónimos que sueñan, se esfuerzan día a día y realizan acciones concretas para transformar su realidad y la de su comunidad."
+	'thirdTextProtas': "Los Protagonistas del Progreso son héroes anónimos que sueñan, se esfuerzan día a día y realizan acciones concretas para transformar su realidad y la de su comunidad.",
+	'ejes': 'Texto explicativo sobre los ejes.'
 }
+
+export default data
