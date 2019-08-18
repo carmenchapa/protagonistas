d32c27686da243f700d62b616a8121da8c43f0f2
Carmen Chapa <cmchapa28@gmail.com>
2019-08-18 03:23:05 +0200

    Pass prota props from data

--- src/protagonistas/protagonista.js
+++ src/protagonistas/protagonista.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import protas from '../data/protagonistas'
 import { MdPlayCircleFilled } from "react-icons/md";
 
 const pink = '#ff8592'
@@ -8,22 +9,21 @@ export default class Protagonista extends React.Component {
 	getHeight = () =>	(window.innerHeight - 144 - 60)
 
   render() {
-
-		const src = require('../static/images/sebas.jpg')
-		const name = 'nombre del prota'
-		const movimiento = 'comuna 13'
-		const text = 'En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor.'
+		// console.log('in prota', this.props.match.params.id)
+		const name = this.props.match.params.id.replace(':', '')
+		const protagonista = protas.find(item => item.name === name)
+		
 		return (
 			<div className="AppContainer" style={{alignContent: 'center'}}>
 					<div className="Protagonista NabBarAvoidingHeight">
 						<div  style={{flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start', padding: 80}}>
 							<p className="Name" >{name.toUpperCase()}</p>
-							<p className="Mov" >{`MOV: ${movimiento.toUpperCase()}`}</p>
-							<p className="Text" >{text}</p>
+							<p className="Mov" >{`MOV: ${protagonista.movimiento.toUpperCase()}`}</p>
+							<p className="Text" >{protagonista.text}</p>
 							<div className="Centred"><h1><MdPlayCircleFilled size='4em' color={pink} /></h1></div>
 						</div>
-						<div className="Centred" height={this.getHeight()} style={{ width: `${this.getHeight()*1.61}`, justifyContent: 'flex-end', padding: 30, paddingTop: 72 + 30}}>
-							<img className="Centred" height='100%' width='100%' src={src} style={{objectFit: "cover"}} alt="" />
+						<div className="Centred" style={{justifyContent: 'flex-end', padding: 30, paddingTop: 72 + 30}}>
+							<img className="Centred" height='100%' width='60%' src={protagonista.img} style={{objectFit: "cover"}} alt="" />
 						</div>
 					</div>
 			</div>
