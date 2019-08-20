e3e0a82e37e9804938a0ec4d7e4471066b256fbf
Carmen Chapa <cmchapa28@gmail.com>
2019-08-21 01:06:20 +0200

    Render pop up with prota

--- src/mapa/index.js
+++ src/mapa/index.js
@@ -1,48 +1,78 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import React from "react"
+import {Map, GoogleApiWrapper, Marker} from "google-maps-react"
+import protas from "../data/protagonistas"
+import {capitalizeFirst} from "../utils/helperFunctions"
 
 class Mapa extends React.Component {
-	state = {
-		stores: [{ latitude: 6.255369, longitude: -75.622766},
-						// {latitude: 47.359423, longitude: -122.021071},
-						// {latitude: 47.2052192687988, longitude: -121.988426208496},
-						// {latitude: 47.6307081, longitude: -122.1434325},
-						// {latitude: 47.3084488, longitude: -122.2140121},
-						{ latitude: 6.215369, longitude: -75.623366}]
-	}
+  state = {
+    // stores: protas.map(e => e.geo),
+    prota: null
+  }
 
-	displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => console.log("You clicked me!")} />
+  displayMarkers = () => {
+    return protas.map((prota, index) => {
+      return (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: prota.geo.latitude,
+            lng: prota.geo.longitude
+          }}
+          onClick={() => this.setState({prota: prota})}
+        />
+      )
     })
   }
 
   render() {
-		return (
-			<div >
-				<Map
+    return (
+      <div>
+        <Map
           google={this.props.google}
           zoom={8}
           style={mapStyles}
-          initialCenter={{ lat: 6.255369, lng: -75.622766}}
+          initialCenter={{lat: 6.255369, lng: -75.622766}}
         >
-					{this.displayMarkers()}
-				</Map>
-			</div>
-			)
+          {this.displayMarkers()}
+        </Map>
+        {this.state.prota && <PopUp prota={this.state.prota} />}
+      </div>
+    )
   }
 }
 
-const mapStyles = {
-  width: '100%',
-  height: '100%',
-};
+const PopUp = props => (
+  <div style={popupStyles}>
+    <img
+      src={props.prota.img}
+      height="80%"
+      width="100%"
+      alt=""
+      style={{objectFit: "cover"}}
+    />
+    <div>
+      <p className="PopupName">{capitalizeFirst(props.prota.name)}</p>
+      <p className="Text">{props.prota.shortText}</p>
+    </div>
+  </div>
+)
 
+const mapStyles = {
+  width: "100%",
+  height: "100%"
+}
+
+const popupStyles = {
+  position: "fixed",
+  padding: "20px",
+  top: "calc(50vh - (60vh / 2))",
+  left: "calc(50vw - (45vh / 2))",
+  width: "45vh",
+  height: "60vh",
+  background: "var(--smokeWhite)"
+}
 
 export default GoogleApiWrapper({
-	apiKey: ''
-})(Mapa);
+  apiKey: process.env.REACT_APP_MAPS_API_KEY
+})(Mapa)
