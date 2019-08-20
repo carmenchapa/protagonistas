575f4b868d1e887fb0faac2bc8dacc22d5cac853
Carmen Chapa <cmchapa28@gmail.com>
2019-08-21 01:05:53 +0200

    Add geolocation to protas

--- src/data/protagonistas.js
+++ src/data/protagonistas.js
@@ -4,28 +4,32 @@ export const protas = [
 		'movimiento': 'comuna 13',
 		'shortText': 'Texto corto',
 		'text': 'Texto ',
-		'img': require('../static/images/protagonistas/perro.jpg')
+		'img': require('../static/images/protagonistas/perro.jpg'),
+		'geo': { latitude: 6.255369, longitude: -75.622766}
 	},
 	{
 		'name': 'aka',
 		'movimiento': 'comuna 13',
 		'shortText': 'Texto corto',
 		'text': 'Texto ',
-		'img': require('../static/images/protagonistas/aka3.jpg')
+		'img': require('../static/images/protagonistas/aka3.jpg'),
+		'geo': { latitude: 6.275369, longitude: -75.622766}
 	},
 	{
 		'name': 'martika',
 		'movimiento': 'comuna 13',
 		'shortText': 'Texto corto',
 		'text': 'Texto ',
-		'img': require('../static/images/protagonistas/martika.jpg')
+		'img': require('../static/images/protagonistas/martika.jpg'),
+		'geo': { latitude: 6.225369, longitude: -75.642766}
 	},
 	{
 		'name': 'Doña Consuelo & Sebastián',
 		'movimiento': 'comuna 13',
 		'shortText': 'Texto corto',
 		'text': 'Texto ',
-		'img': require('../static/images/protagonistas/cremas.jpg')
+		'img': require('../static/images/protagonistas/cremas.jpg'),
+		'geo': { latitude: 6.555369, longitude: -75.122766}
 	}
 ]
 
