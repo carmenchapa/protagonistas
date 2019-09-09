121b75d3dc54541edbecb95d01ed3550a790dd5f
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-09 15:28:15 +0200

    Add protas texts

--- src/data/protagonistas.js
+++ src/data/protagonistas.js
@@ -1,36 +1,49 @@
 export const protas = [
-	{
-		'name': 'perro',
-		'movimiento': 'comuna 13',
-		'shortText': 'Texto corto',
-		'text': 'Texto ',
-		'img': require('../static/images/protagonistas/perro.jpg'),
-		'geo': { latitude: 6.255369, longitude: -75.622766}
-	},
-	{
-		'name': 'aka',
-		'movimiento': 'comuna 13',
-		'shortText': 'Texto corto',
-		'text': 'Texto ',
-		'img': require('../static/images/protagonistas/aka3.jpg'),
-		'geo': { latitude: 6.275369, longitude: -75.622766}
-	},
-	{
-		'name': 'martika',
-		'movimiento': 'comuna 13',
-		'shortText': 'Texto corto',
-		'text': 'Texto ',
-		'img': require('../static/images/protagonistas/martika.jpg'),
-		'geo': { latitude: 6.225369, longitude: -75.642766}
-	},
-	{
-		'name': 'Doña Consuelo & Sebastián',
-		'movimiento': 'comuna 13',
-		'shortText': 'Texto corto',
-		'text': 'Texto ',
-		'img': require('../static/images/protagonistas/cremas.jpg'),
-		'geo': { latitude: 6.555369, longitude: -75.122766}
-	}
-]
+  {
+    name: "perro",
+    movimiento: "comuna 13",
+    shortText: "Texto corto",
+    text: `También conocido, aunque menos, como Daniel Felipe Quiceno. Es artista, es grafitero. Tiene 29 años.
+    El Perro quería ser uno de los mejores grafiteros de Medellín y ahí está: es uno de los fundadores del grafitour de la C13.
+    El arte es su manera de tramitar la realidad.
+    “Turismo comunitario” es como define el movimiento alrededor de la apropiación de color en las paredes del barrio. Al grafitour, mal contadas, asisten dos mil personas por día.
+    Y está orgulloso de lo que hace. Vive del arte hace 10 años.`,
+    img: require("../static/images/protagonistas/perro.jpg"),
+    geo: { latitude: 6.255369, longitude: -75.622766 }
+  },
+  {
+    name: "aka",
+    movimiento: "comuna 13",
+    shortText: "Texto corto",
+    text: `Un sembrador, eso es el Aka. De procesos, de semillas, de preguntas.
+      Este joven activista entiende la agricultura como un todo, un punto cero adonde todo regresa. Por eso un día está plantando con comunidades o en ejercicios de memoria colectiva, siempre desde el territorio.
+      Y otro día está haciendo canciones, porque el Aka también es hip hop. Y trabaja con jóvenes y con mayores y con todos. También es artista. Es incansable.`,
+    img: require("../static/images/protagonistas/aka3.jpg"),
+    geo: { latitude: 6.275369, longitude: -75.622766 }
+  },
+  {
+    name: "martika",
+    movimiento: "comuna 13",
+    shortText: "Texto corto",
+    text: `Marta, Martica o Lulú: la misma persona hiperactiva y entregada a labores sociales que desde hace 10 años coordina la fundación Culturizzarte en Belencito, en Medellín. Un proyecto para encausar jóvenes en condición de vulnerabilidad desde el arte y la cultura.
+      Martica está en lo social desde niña. ¨No pasar por pasar¨ es algo así como su mantra.
+      Es santandereana y ha vivido en muchas partes de Colombia. Sueña con crear un instituto de artes y oficios para la comuna 13.
+      NO, la palabra que no figura en su diccionario personal.`,
+    img: require("../static/images/protagonistas/martika.jpg"),
+    geo: { latitude: 6.225369, longitude: -75.642766 }
+  },
+  {
+    name: "sebastián",
+    movimiento: "comuna 13",
+    shortText: "Texto corto",
+    text: `25 años apenas y, sin embargo, muchos de ellos como emprendedor. Desde niño está trabajando, inventando.
+      El negocio de helados familiar, dicen, fue el primero que visitó un turista en la comuna 13. Lo que sí es un hecho es que es el que da la bienvenida a los recorridos turísticos de las escaleras mecánicas.
+      Doña Consuelo es el nombre, en honor a la abuela, la de la receta: son helados de fruta natural.
+      Como es familiar, ahí está el padre, don Jaime, el que atiende. Y el abuelo, el que compra la fruta.
+      Sebastián se mueve de aquí para allá. Es el administrador detrás de todo, y quien resalta el valor de la terquedad.`,
+    img: require("../static/images/protagonistas/cremas.jpg"),
+    geo: { latitude: 6.555369, longitude: -75.122766 }
+  }
+];
 
-export default protas
+export default protas;
