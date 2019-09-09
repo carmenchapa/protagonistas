f45eb50d6d07230bedc68c0c72e10b582f4c7a6a
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-09 17:26:22 +0200

    Add newText helper function

--- src/home/index.js
+++ src/home/index.js
@@ -1,48 +1,87 @@
-import React from 'react';
-import '../static/images/map.png'
-import data from '../data/generalTexts'
+import React from "react";
+import "../static/images/map.png";
+import data from "../data/generalTexts";
+import { newText } from "../utils/helperFunctions";
 
 export default class Home extends React.Component {
   render() {
-		return (
-			<div className="HomeContainer" >
-				<div className="HomeBackground NabBarAvoiding">
-					<p className="HomeTitle">PROTAGONISTAS <br/>DEL PROGRESO</p>
-				</div>
-				<div className="FullHeight FlexStyle" style={{flexDirection: 'column', flex: 1, alignSelf: 'center'}}>
-					<div style={secondContainerStyle}>
-						<Square text='¿Qué es Protagonistas del progreso?' color='#d8d5bd' size={'45vh'} fontSize={50}/>
-						<Rectangle src={require('../static/images/map.png')} width={'55vh'} height={'40vh'} />
-					</div>
-					
-				</div> 
-				<div className="FullHeight FlexStyle" style={thirdContainerStyle}>
-					<p>{data.introProtas}</p>
-				</div>
-			</div>
-			)
+    return (
+      <div className="HomeContainer">
+        <div className="HomeBackground NabBarAvoiding">
+          <p className="HomeTitle">
+            PROTAGONISTAS <br />
+            DEL PROGRESO
+          </p>
+        </div>
+        <div
+          className="FullHeight FlexStyle"
+          style={{ flexDirection: "column", flex: 1, alignSelf: "center" }}
+        >
+          <div style={secondContainerStyle}>
+            <Square
+              text="¿Qué es Protagonistas del progreso?"
+              color="#d8d5bd"
+              size={"45vh"}
+              fontSize={50}
+            />
+            <Rectangle
+              src={require("../static/images/map.png")}
+              width={"55vh"}
+              height={"40vh"}
+            />
+          </div>
+        </div>
+        <div className="FullHeight FlexStyle" style={thirdContainerStyle}>
+          <p>{newText(data.introProtas)}</p>
+        </div>
+      </div>
+    );
   }
 }
 
 const secondContainerStyle = {
-	display: 'flex',
-	flex: 1,
-	flexDirection: 'row',
-	justifyContent: 'space-around',
-	alignItems: 'flex-end',
-	alignContent: 'center',
-	margin: 'auto',
-	marginLeft: '5%',
-	marginRight: '5%',
-	maxHeight: '45vh'
-}
+  display: "flex",
+  flex: 1,
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "flex-end",
+  alignContent: "center",
+  margin: "auto",
+  marginLeft: "5%",
+  marginRight: "5%",
+  maxHeight: "45vh"
+};
 
 const thirdContainerStyle = {
-	marginLeft: '20vw',
-	marginRight: '20vw',
-	fontSize: '1.4rem'
-}
+  marginLeft: "20vw",
+  marginRight: "20vw",
+  fontSize: "1.4rem"
+};
 
-const Square = (props) => <div className="Centred" style={{backgroundColor: props.color, width: props.size, height: props.size, padding: 40}}><p style={{fontSize: props.fontSize}}>{props.text}</p></div>
+const Square = props => (
+  <div
+    className="Centred"
+    style={{
+      backgroundColor: props.color,
+      width: props.size,
+      height: props.size,
+      padding: 40
+    }}
+  >
+    <p style={{ fontSize: props.fontSize }}>{props.text}</p>
+  </div>
+);
 
-const Rectangle = (props) => <div className="Centred" style={{backgroundColor: props.color, width: props.width, height: props.height, alignItems: 'flex-end'}}><img src={props.src} width={'100%'} height={'100%'} alt="" /></div>
+const Rectangle = props => (
+  <div
+    className="Centred"
+    style={{
+      backgroundColor: props.color,
+      width: props.width,
+      height: props.height,
+      alignItems: "flex-end"
+    }}
+  >
+    <img src={props.src} width={"100%"} height={"100%"} alt="" />
+  </div>
+);
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,4 +1,16 @@
-export function capitalizeFirst(value: string) {
-	const newValue = value.toLowerCase()
-  return newValue.charAt(0).toUpperCase() + newValue.slice(1)
+import React from "react";
+
+export function capitalizeFirst(value) {
+  const newValue = value.toLowerCase();
+  return newValue.charAt(0).toUpperCase() + newValue.slice(1);
+}
+
+export function newText(text) {
+  return text.split("\n").map((item, i) => {
+    return (
+      <p key={i} className="Text">
+        {item}
+      </p>
+    );
+  });
 }
