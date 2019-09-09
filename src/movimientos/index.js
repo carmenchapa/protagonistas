6b801d7ddcd377bd9ec654f38db3bbd73407b58a
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-09 17:28:46 +0200

    Format

--- src/movimientos/index.js
+++ src/movimientos/index.js
@@ -1,55 +1,63 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import movimientos from '../data/movimientos'
+import movimientos from "../data/movimientos";
 
 export default class Movimientos extends React.Component {
-	getWidth = () =>	3 * (window.innerWidth*0.28 + 40)
+  getWidth = () => 3 * (window.innerWidth * 0.28 + 40);
   render() {
-		return (
-			<div className="AppContainer NabBarAvoiding">
-				<div style={{...gridStyle, maxWidth: `${this.getWidth()}px`}}>
-				{movimientos.map((e, i) => 
-					<Movimiento key={i} width={'28vw'} height={'20vw'} name={e.nombre} src={e.img} 
-					description={e.shortText}/>
-				)}
-			</div>
-			</div>
-			)
+    return (
+      <div className="AppContainer NabBarAvoiding">
+        <div style={{ ...gridStyle, maxWidth: `${this.getWidth()}px` }}>
+          {movimientos.map((e, i) => (
+            <Movimiento
+              key={i}
+              width={"28vw"}
+              height={"20vw"}
+              name={e.nombre}
+              src={e.img}
+              description={e.shortText}
+            />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
 const gridStyle = {
-	display: 'flex',
-	// maxWidth: '1200px',
-  margin: '0 auto',
-	flexDirection: 'row',
-	justifyContent: 'flex-start',
-	flexWrap: 'wrap'
-}
+  display: "flex",
+  // maxWidth: '1200px',
+  margin: "0 auto",
+  flexDirection: "row",
+  justifyContent: "flex-start",
+  flexWrap: "wrap"
+};
 
 const movimientoContainer = (width, height, src) => {
-	return {
-		display: 'flex',
-		width: width,
-		height: height,
-		overflow: 'hidden',
-		backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.6)60%, rgba(0, 0, 0, 0.95)100%),
+  return {
+    display: "flex",
+    width: width,
+    height: height,
+    overflow: "hidden",
+    backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.6)60%, rgba(0, 0, 0, 0.95)100%),
 		url(${src})`,
-		backgroundSize: 'contain',
-		margin: 20,
-		alignItems: 'flex-end'
-}
-}
+    backgroundSize: "contain",
+    margin: 20,
+    alignItems: "flex-end"
+  };
+};
 
-
-const Movimiento = (props) =>
-<Link className="link"
-	to={{	pathname: `/protagonistas:id`}}
-	>
-		<div style={movimientoContainer(props.width, props.height, props.src)}>
-			<div style={{flexDirection: 'row', margin: 10, color: 'var(--gold)'}}>
-			<p className="ProtagonistaName">{props.name.toString().toUpperCase()}</p>
-			<p className="ProtagonistaDescription" style={{color: 'OldLace'}}>{props.description.toString().toUpperCase()}</p>
-			</div>
-		</div>
-		</Link>
+const Movimiento = props => (
+  <Link className="link" to={{ pathname: `/protagonistas:id` }}>
+    <div style={movimientoContainer(props.width, props.height, props.src)}>
+      <div style={{ flexDirection: "row", margin: 10, color: "var(--gold)" }}>
+        <p className="ProtagonistaName">
+          {props.name.toString().toUpperCase()}
+        </p>
+        <p className="ProtagonistaDescription" style={{ color: "OldLace" }}>
+          {props.description.toString().toUpperCase()}
+        </p>
+      </div>
+    </div>
+  </Link>
+);
