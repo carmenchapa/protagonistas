bd0b0d9288a146a0bd31c5b2a4545386eb7271a6
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-09 16:34:12 +0200

    Update texts

--- src/data/generalTexts.js
+++ src/data/generalTexts.js
@@ -1,8 +1,11 @@
 const data = {
-	'introProtas': `Protagonistas del Progreso es un movimiento transmedia con fines sociales, que a través de diferentes medios busca visibilizar la vida de personas en todo el mundo, comenzando por América Latina, que, con sus actos cotidianos logran transformar su realidad personal, de las personas que les rodean, y de su comunidad de forma extraordinaria. El movimiento pretende resaltar y darle importancia a las historias de vida de héroes anónimos - Protagonistas del Progreso - para que estos se conviertan en los modelos a seguir de las futuras generaciones.`,
-	'subTextProtas': "El movimiento de los Protagonistas del Progreso quiere visibilizar las historias de vida de héroes cotidianos, rompiendo con el paradigma de los modelos que inspiran actualmente.",
-	'thirdTextProtas': "Los Protagonistas del Progreso son héroes anónimos que sueñan, se esfuerzan día a día y realizan acciones concretas para transformar su realidad y la de su comunidad.",
-	'ejes': 'Texto explicativo sobre los ejes.'
-}
+  introProtas: `Protagonistas del Progreso es un movimiento transmedia con fines sociales, que a través de diferentes medios busca visibilizar la vida de personas en todo el mundo, comenzando por América Latina, que, con sus actos cotidianos logran transformar su realidad personal, de las personas que les rodean, y de su comunidad de forma extraordinaria. El movimiento pretende resaltar y darle importancia a las historias de vida de héroes anónimos - Protagonistas del Progreso - para que estos se conviertan en los modelos a seguir de las futuras generaciones.`,
+  subTextProtas:
+    "El movimiento de los Protagonistas del Progreso quiere visibilizar las historias de vida de héroes cotidianos, rompiendo con el paradigma de los modelos que inspiran actualmente.",
+  thirdTextProtas:
+    "Los Protagonistas del Progreso son héroes anónimos que sueñan, se esfuerzan día a día y realizan acciones concretas para transformar su realidad y la de su comunidad.",
+  ejes: `Las diferentes dimensiones en las cuales el movimiento tiene líneas de acción.
+  Cada uno de ellos tiene un ecosistema y ciertas complejidades propias.`
+};
 
-export default data
+export default data;
