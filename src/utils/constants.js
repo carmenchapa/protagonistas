1652c8453694e5016a218cb35d6a57f2a41c9675
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-11 17:03:40 +0200

    Add constants file with colors

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+// ------* COLORS *------ //
+
+export const SMOKE_WHITE = "#f7f7f7";
+export const GOLD = "#d8d5bd";
+export const AQUAMARINE = "#95dce1";
+export const PINK = "#ff8592";
+export const CYAN = "#73c0d3";
+export const GREEN = "#8ee095";
+export const DARKBLACK = "#1e1e1e";
