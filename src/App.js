2a9402b0e8319691d47757395c6af7e2673ac871
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-11 21:18:16 +0200

    Use hooks for responsiveness

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as ROUTES from "./navigation/routes";
 import Navigation from "./navigation";
+import WindowDimensionsProvider from "./components/WindowDimensionsProvider";
 import Home from "./home";
 import Movimientos from "./movimientos";
 import Movimiento from "./movimientos/movimiento";
@@ -14,22 +15,24 @@ function App() {
   // console.log('App', this.context)
   const { pathname } = window.location;
   return (
-    <Router>
-      <div className="AppContainer">
-        <Navigation currentRoute={pathname} />
+    <WindowDimensionsProvider>
+      <Router>
         <div className="AppContainer">
-          <Route exact path={ROUTES.HOME[0]} component={Home} />
-          <Route path={ROUTES.MOVIMIENTOS[0]} component={Movimientos} />
-          <Route path={ROUTES.PROTAGONISTAS[0]} component={Protagonistas} />
-          <Route path={ROUTES.EJES[0]} component={Ejes} />
-          <Route path={ROUTES.MAPA[0]} component={Mapa} />
-          <Route path={ROUTES.PROTAGONISTA[0]} component={Protagonista} />
-          <Route path={ROUTES.MOVIMIENTO[0]} component={Movimiento} />
+          <Navigation currentRoute={pathname} />
+          <div className="AppContainer">
+            <Route exact path={ROUTES.HOME[0]} component={Home} />
+            <Route path={ROUTES.MOVIMIENTOS[0]} component={Movimientos} />
+            <Route path={ROUTES.PROTAGONISTAS[0]} component={Protagonistas} />
+            <Route path={ROUTES.EJES[0]} component={Ejes} />
+            <Route path={ROUTES.MAPA[0]} component={Mapa} />
+            <Route path={ROUTES.PROTAGONISTA[0]} component={Protagonista} />
+            <Route path={ROUTES.MOVIMIENTO[0]} component={Movimiento} />
 
-          {/* <Route path="/proposals:id" component={DecisionPage} /> */}
+            {/* <Route path="/proposals:id" component={DecisionPage} /> */}
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </WindowDimensionsProvider>
   );
 }
 
