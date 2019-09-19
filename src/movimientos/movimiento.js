87603f4d68e7d2f2097e86b4a19b2ac692385b51
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-19 16:54:37 +0200

    Format

--- src/movimientos/movimiento.js
+++ src/movimientos/movimiento.js
@@ -1,47 +1,41 @@
-import React from "react";
-import movimientos from "../data/movimientos";
-import { MdPlayCircleFilled } from "react-icons/md";
-import { newText } from "../utils/helperFunctions";
-
-const pink = "#ff8592";
+import React from 'react'
+import movimientos from '../data/movimientos'
+import { MdPlayCircleFilled } from 'react-icons/md'
+import { newText } from '../utils/helperFunctions'
+import * as constants from '../utils/constants'
 
 export default class Movimiento extends React.Component {
-  getHeight = () => window.innerHeight - 144 - 60;
-
   render() {
-    console.log("in prota", this.props.match.params.id);
-    const name = this.props.match.params.id.replace(":", "");
-    const movimiento = movimientos.find(item => item.name === name);
-    console.log("name", name, movimiento);
+    // console.log("in prota", this.props.match.params.id);
+    const name = this.props.match.params.id.replace(':', '')
+    const movimiento = movimientos.find(item => item.name === name)
+    // console.log("name", name, movimiento);
     return (
-      <div className="AppContainer" style={{ alignContent: "center" }}>
+      <div className='AppContainer' style={{ alignContent: 'center' }}>
         <div
-          className="Movimiento NabBarAvoidingHeight"
+          className='Movimiento NabBarAvoidingHeight'
           style={{
             backgroundImage: `url(${movimiento.img})`,
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "right bottom"
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'right bottom'
           }}
         >
-          <div className="MovimientoFichaContainer">
-            <p className="Name">{name.toUpperCase()}</p>
-            {/* <p className="Mov">{`MOV: ${movimientos.movimiento.toUpperCase()}`}</p> */}
+          <div className='MovimientoFichaContainer'>
+            <p className='Name'>{name.toUpperCase()}</p>
             {newText(movimiento.text)}
-            <div className="Centred">
+            <div className='Centred'>
               <h1>
-                <MdPlayCircleFilled size="4em" color={pink} />
+                <MdPlayCircleFilled size='4em' color={constants.PINK} />
               </h1>
             </div>
           </div>
-          <div className="Centred MovimientoImageContainer">
-            {/* <img
+          <div className='Centred MovimientoImageContainer'>{/* <img
               className="Centred MovimientoImage"
               src={movimiento.img}
               alt=""
-            /> */}
-          </div>
+            /> */}</div>
         </div>
       </div>
-    );
+    )
   }
 }
