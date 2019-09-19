fa2b2d85e31b5c7454b8fdb57971bd5a8ba1459c
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-19 11:46:12 +0200

    Add social media option for square

--- src/components/CommonComponents.js
+++ src/components/CommonComponents.js
@@ -1,9 +1,25 @@
-import React from "react";
-import { newText } from "../utils/helperFunctions";
+import React from 'react'
+import { newText } from '../utils/helperFunctions'
+import { FaInstagram, FaFacebook } from 'react-icons/fa'
 
+const imageContainer = src => {
+  return {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    backgroundImage: `url(${src})`,
+    backgroundSize: 'cover',
+    alignItems: 'flex-end'
+  }
+}
+const iconProps = {
+  size: '1.8em',
+  color: 'white',
+  style: { padding: 8, marginLeft: 4 }
+}
 export const Square = props => (
   <div
-    className="Centred"
+    className='Centred'
     style={{
       backgroundColor: props.color,
       width: props.size,
@@ -11,28 +27,36 @@ export const Square = props => (
       ...props.style
     }}
   >
-    {props.text && (
-      <div style={{ padding: 40, fontSize: props.fontSize }}>
-        {/* <p style={{ fontSize: props.fontSize }}>{props.text}</p> */}
-        {newText(props.text)}
+    {props.text && <div style={{ padding: 40, fontSize: props.fontSize }}>{newText(props.text)}</div>}
+    {props.src && (
+      <div style={imageContainer(props.src)}>
+        {props.inst && (
+          <a href={props.inst} target='blank'>
+            <FaInstagram {...iconProps} />
+          </a>
+        )}
+        {props.facebook && (
+          <a href={props.inst} target='blank'>
+            <FaFacebook {...iconProps} />
+          </a>
+        )}
       </div>
     )}
-    {props.src && <img className="cover" src={props.src} alt="" />}
   </div>
-);
+)
 
 export const Rectangle = props => (
   <div
-    className="Centred"
+    className='Centred'
     style={{
       backgroundColor: props.color,
       width: props.width,
       height: props.height,
-      alignItems: "flex-end",
-      border: "none",
+      alignItems: 'flex-end',
+      border: 'none',
       ...props.style
     }}
   >
-    {props.src && <img className="cover" src={props.src} alt="" />}
+    {props.src && <img className='cover' src={props.src} alt='' />}
   </div>
-);
+)
