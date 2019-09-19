7ada782bd721303261636203be13cbea4392569b
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-19 16:49:44 +0200

    Format

--- src/data/movimientos.js
+++ src/data/movimientos.js
@@ -1,12 +1,12 @@
 const movimientos = [
   {
-    name: "comuna13",
-    protagonistas: ["perro", "aka", "martika", "cremas"],
-    shortText: "Medellín, Colombia",
+    name: 'comuna13',
+    protagonistas: ['perro', 'aka', 'martika', 'cremas'],
+    shortText: 'Medellín, Colombia',
     text: `La comuna 13 ha sido reconocida internacionalmente como un referente de urbanismo social en pro de la transformación y la revitalización de la comunidad.  Se han gestado un sinnúmero de iniciativas que buscan evidenciar esas ganas de luchar contra la violencia que tanto los ha golpeado por años.
     La “Comuna 13 Graffitour” es uno de los ejemplos de transformación desde el arte. Se trata de un recorrido guiado por PERROGRAFF y su equipo, el cual busca analizar cómo a través de la música y el arte la comunidad se ha ido sanando e integrando, recibiendo día tras día a cientos de turistas y locales que admiran y se deleitan con el arte y sus historias.`,
-    img: require("../static/images/movimientos/aka1.png")
+    img: require('../static/images/movimientos/aka1.png')
   }
-];
+]
 
-export default movimientos;
+export default movimientos
