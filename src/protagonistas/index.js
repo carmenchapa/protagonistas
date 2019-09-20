029d57648057a1917998dae548af61df32ac7757
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-20 15:53:02 +0200

    Scroll up Protagonistas page on load

--- src/protagonistas/index.js
+++ src/protagonistas/index.js
@@ -1,26 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import protas from "../data/protagonistas";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import protas from '../data/protagonistas'
 
-const Protagonistas = () => (
-  <div className="AppContainer NabBarAvoiding ProtasContainer ListContainer gridProtas">
+export default class Protagonistas extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
+  render() {
+    return (
+      <div className='AppContainer NabBarAvoiding ListContainer Dark'>
+        <ProtagonistasList />
+      </div>
+    )
+  }
+}
+
+export const ProtagonistasList = () => (
+  <div className='gridProtas'>
     {protas.map((e, i) => (
       <Prota key={i} name={e.name} src={e.img} description={e.shortText} />
     ))}
   </div>
-);
+)
 
 const protaContainer = src => {
   return {
-    paddingTop: "85%",
+    paddingTop: '85%',
     backgroundImage: `url(${src})`,
-    backgroundSize: "cover"
-  };
-};
+    backgroundSize: 'cover'
+  }
+}
 
 const Prota = props => (
   <Link
-    className="link ProtasContainer"
+    className='link ProtasContainer'
     to={{
       pathname: `/protagonistas:${props.name}`,
       protagonista: props.name
@@ -28,10 +41,8 @@ const Prota = props => (
   >
     <div>
       <div style={protaContainer(props.src)} />
-      <p className="ProtagonistaName">{props.name.toUpperCase()}</p>
-      <p className="ProtagonistaDescription">{props.description}</p>
+      <p className='ProtagonistaName'>{props.name.toUpperCase()}</p>
+      <p className='ProtagonistaDescription'>{props.description}</p>
     </div>
   </Link>
-);
-
-export default Protagonistas;
+)
