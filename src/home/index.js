2ae682d996c265ede3cf130a6714d957fe808559
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-20 15:52:11 +0200

    Format

--- src/home/index.js
+++ src/home/index.js
@@ -1,61 +1,50 @@
-import React from "react";
-import "../static/images/map.png";
-import data from "../data/generalTexts";
-import { newText } from "../utils/helperFunctions";
-import { Square, Rectangle } from "../components/CommonComponents";
+import React from 'react'
+import '../static/images/map.png'
+import data from '../data/generalTexts'
+import { newText } from '../utils/helperFunctions'
+import { Square, Rectangle } from '../components/CommonComponents'
 
 export default class Home extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
   render() {
     return (
-      <div className="HomeContainer">
-        <div className="HomeBackground NabBarAvoiding">
-          <p className="HomeTitle">
+      <div className='HomeContainer'>
+        <div className='HomeBackground NabBarAvoiding'>
+          <p className='HomeTitle'>
             PROTAGONISTAS <br />
             DEL PROGRESO
           </p>
         </div>
-        <div
-          className="FullHeight FlexStyle"
-          style={{ flexDirection: "column", flex: 1, alignSelf: "center" }}
-        >
+        <div className='FullHeight FlexStyle' style={{ flexDirection: 'column', flex: 1, alignSelf: 'center' }}>
           <div style={secondContainerStyle}>
-            <Square
-              text="¿Qué es Protagonistas del progreso?"
-              color="#d8d5bd"
-              size={"45vh"}
-              fontSize={50}
-            />
-            <Rectangle
-              src={require("../static/images/map.png")}
-              width={"55vh"}
-              height={"35vh"}
-            />
+            <Square text='¿Qué es Protagonistas del progreso?' color='#d8d5bd' size={'45vh'} fontSize={50} />
+            <Rectangle src={require('../static/images/map.png')} width={'55vh'} height={'35vh'} />
           </div>
         </div>
-        <div className="FullHeight FlexStyle" style={thirdContainerStyle}>
+        <div className='FullHeight FlexStyle' style={thirdContainerStyle}>
           {newText(data.introProtas)}
         </div>
       </div>
-    );
+    )
   }
 }
 
 const secondContainerStyle = {
-  display: "flex",
+  display: 'flex',
   flex: 1,
-  flexDirection: "row",
-  justifyContent: "space-around",
-  alignItems: "flex-end",
-  alignContent: "center",
-  margin: "auto",
-  marginLeft: "5%",
-  marginRight: "5%",
-  maxHeight: "45vh"
-};
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems: 'flex-end',
+  margin: 'auto',
+  marginLeft: '5%',
+  marginRight: '5%',
+  maxHeight: '45vh'
+}
 
 const thirdContainerStyle = {
-  marginLeft: "25vw",
-  marginRight: "25vw",
-  fontSize: "1.2vw"
-  // fontSize: "calc(1.2vw+4px)"
-};
+  marginLeft: '25vw',
+  marginRight: '25vw',
+  fontSize: '1.2vw'
+}
