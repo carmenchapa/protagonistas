1e6ae8fd3cd82e6678b272fb9dd0f3e00ecee4d7
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-09-22 00:23:01 +0200

    Fix ejes styles

--- src/ejes/index.js
+++ src/ejes/index.js
@@ -1,44 +1,48 @@
-import React from 'react';
+import React from 'react'
 // import data from '../data/generalTexts'
 
 export default class Ejes extends React.Component {
   render() {
-		return (
-			<div className="AppContainer NabBarAvoiding">
-				<div className="FullHeight FlexStyle" style={containerStyle}>
-					{/* <p>{data.ejes}</p> */}
-					<div className="FlexStyle Row">
-						<div className="FlexStyle" style={cyan}></div>
-						<div className="FlexStyle" style={pink}></div>
-					</div>
+    return (
+      <div className='AppContainer NabBarAvoiding'>
+        <div className=' gridPadding gridEjes'>
+          {/* <p>{data.ejes}</p> */}
+          {/* <div className="FlexStyle Row"> */}
+          <div className='FlexStyle' style={cyan}></div>
+          <div className='FlexStyle' style={pink}></div>
+          {/* </div> */}
 
-					<div className="FlexStyle Row">
-						<div className="FlexStyle" style={green}></div>
-						<div className="FlexStyle" style={aquamarine}></div>
-					</div>
-				</div>
-			</div>
-			)
+          {/* <div className="FlexStyle Row"> */}
+          <div className='FlexStyle' style={green}></div>
+          <div className='FlexStyle' style={aquamarine}></div>
+          {/* </div> */}
+        </div>
+      </div>
+    )
   }
 }
 
 const pink = {
-	backgroundColor: 'var(--pink)'
+  paddingTop: '100%',
+  backgroundColor: 'var(--pink)'
 }
 const cyan = {
-	backgroundColor: 'var(--cyan)'
+  paddingTop: '100%',
+  backgroundColor: 'var(--cyan)'
 }
 const aquamarine = {
-	backgroundColor: 'var(--aquamarine)'
+  paddingTop: '100%',
+  backgroundColor: 'var(--aquamarine)'
 }
 const green = {
-	backgroundColor: 'var(--green)'
+  paddingTop: '100%',
+  backgroundColor: 'var(--green)'
 }
 
 const containerStyle = {
-	display: 'flex',
-	flexDirection: 'column',
-	marginLeft: '18vw',
-	marginRight: '18vw',
-	fontSize: '1.4rem'
+  display: 'flex',
+  flexDirection: 'column',
+  marginLeft: '18vw',
+  marginRight: '18vw',
+  fontSize: '1.4rem'
 }
