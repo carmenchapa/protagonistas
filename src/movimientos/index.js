de27d826d4df017791d01b25791f2bc29941cabd
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-10-09 14:00:35 +0200

    Adjust mov title style

--- src/movimientos/index.js
+++ src/movimientos/index.js
@@ -14,7 +14,7 @@ const Movimientos = () => (
 const Movimiento = props => (
   <Link className='link' to={{ pathname: `/movimientos:${props.name}`, movimiento: props.name }}>
     <div className='MovListImgContainer' style={GradientBackgroundImage(props.src)}>
-      <div style={{ flexDirection: 'row', margin: 10, color: 'var(--gold)' }}>
+      <div style={{ flexDirection: 'row', padding: '1em', paddingBottom: 1, color: 'var(--gold)' }}>
         <p className='ProtagonistaName'>{props.name.toString().toUpperCase()}</p>
         <p className='ProtagonistaDescription' style={{ color: 'OldLace' }}>
           {props.description.toString().toUpperCase()}
