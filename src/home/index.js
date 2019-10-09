3f4bb1430948bb3c3abfb4b44233fe94dbb0a0a6
Pedro Alvarez <pedroalvarez@Pedros-MacBook-Pro-2.local>
2019-10-09 13:50:23 +0200

    Change about

--- src/home/index.js
+++ src/home/index.js
@@ -17,7 +17,17 @@ export default class Home extends React.Component {
             DEL PROGRESO
           </p>
         </div>
-        <div className='FullHeight FlexStyle' style={{ flexDirection: 'column', flex: 1, alignSelf: 'center' }}>
+        <div className='gridPadding'>
+          <div style={{ paddingTop: '4%', paddingBottom: '4%' }}>{newText(data.introProtas)}</div>
+          <div className='gridEjes'>
+            <EjeSquare text={'manifiesto'.toUpperCase()} color='cyan' />
+            <EjeSquare text={'protagonistas'.toUpperCase()} color='pink' />
+            {/* <EjeSquare text='arte' color={green} />
+          <EjeSquare text='ciencia' color={aquamarine} /> */}
+          </div>
+        </div>
+
+        {/* <div className='FullHeight FlexStyle' style={{ flexDirection: 'column', flex: 1, alignSelf: 'center' }}>
           <div style={secondContainerStyle}>
             <Square text='¿Qué es Protagonistas del progreso?' color='#d8d5bd' size={'45vh'} fontSize={50} />
             <Rectangle src={require('../static/images/map.png')} width={'55vh'} height={'35vh'} />
@@ -25,10 +35,34 @@ export default class Home extends React.Component {
         </div>
         <div className='FullHeight FlexStyle' style={thirdContainerStyle}>
           {newText(data.introProtas)}
-        </div>
+        </div> */}
       </div>
     )
   }
+}
+
+const EjeSquare = props => (
+  <div className={`FlexStyle Centred pT100 ${props.color}`}>
+    <div style={inside}>
+      <img src={props.img} alt='' />
+      <p className='Mov' style={{ color: '#fff' }}>
+        {props.text}
+      </p>
+    </div>
+  </div>
+)
+
+const inside = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: '-100%'
+}
+
+const pT100 = {
+  paddingTop: '100%'
+  // backgroundColor: 'var(--pink)'
 }
 
 const secondContainerStyle = {
