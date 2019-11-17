bed28dac3c9195129c9a2841d7745a4428afa5fb
Carmen Chapa <cmchapa28@gmail.com>
2019-11-18 00:10:11 +0100

    Update progonistas data

--- src/data/protagonistas.js
+++ src/data/protagonistas.js
@@ -3,9 +3,9 @@ const protas = [
     name: "perro",
     fullName: `Daniel Felipe Quiceno, Medellín, Colombia`,
     movimiento: "comuna13",
-    shortText:
-      "Cofundador del “Graffitour” Comuna 13, habitante del barrio San Javier.",
-    text: `Artista urbano, graffitero por pasión, arquitecto por vocación, encuentra en elarte la mejor forma de relacionarse con el espacio y la comunidad.
+    shortText: "Cofundador del “Graffitour",
+    text: `Cofundador del “Graffitour” Comuna 13, habitante del barrio San Javier.
+		Artista urbano, graffitero por pasión, arquitecto por vocación, encuentra en elarte la mejor forma de relacionarse con el espacio y la comunidad.
 		A pesar de estar inmerso en la violencia que azotó la comuna por muchosaños, decidió montar un   “parche” (una iniciativa popular) que incluyerajóvenes y mayores, con el objetivo de brindar oportunidades a todos loshabitantes del barrio. Gracias a esta iniciativa el turismo comunitario es unestilo de vida que les permite avanzar y mostrar al mundo su talento.
 		Él mismo se describe como un narrador de historias, las cuales cuenta a travésde sus grafitis y los de sus amigos. Sabe que un graffiti no es la solución atodos los problemas de su comunidad, pero sabe que a través de estos se logra llamar la atención del gobierno, la empresa privada y la ciudadanía, parasumar acciones y lograr un impacto mayor.`,
     img: require("../static/images/protagonistas/perro.jpg"),
@@ -16,7 +16,7 @@ const protas = [
     name: "aka",
     fullName: `Luis Fernando Álvarez, Medellín, Colombia`,
     movimiento: "comuna13",
-    shortText: "Sembrador y músico de Medellín",
+    shortText: "Cofundador del movimiento Hip Hop-Agrario - AgroArte",
     text: `Cofundador del movimiento Hip Hop-Agrario - AgroArte de la comuna 13,habitante del barrio San Javier.
 		Agricultor por pasión, rapero por vocación, encontró en la tierra la memoriacolectiva de su comunidad, por eso a través de procesos de siembra él y sugrupo, hablan, reviven historias y componen las letras de Hip Hop Agrario quepermiten contar las vivencias de todo un barrio.
 		Sus historias no sólo están en boca de todos, sino que están en cada acción, encada maceta, en cada planta, y sobre todo se escuchan a través de suscanciones. Para él las semillas guardan memoria, y la única forma de superarun pasado violento es hablándolo, enfrentándolo y comprometiéndose atrabajar en comunidad. A través de la ciencia, lleva a la gente de la comunaseguridad alimentaria, actualmente muchos habitantes se benefician de lashuertas ornamentales, aromáticas y medicinales.`,
@@ -25,7 +25,7 @@ const protas = [
   },
   {
     name: "martika",
-    fullName: ``,
+    fullName: `Martika`,
     movimiento: "comuna13",
     shortText: "Educadora social de Medellín",
     text: `Marta, Martica o Lulú: la misma persona hiperactiva y entregada a labores sociales que desde hace 10 años coordina la fundación Culturizzarte en Belencito, en Medellín. Un proyecto para encausar jóvenes en condición de vulnerabilidad desde el arte y la cultura.
@@ -39,8 +39,9 @@ const protas = [
     name: "sebas",
     fullName: `Sebastián y Doña Consuelo, Medellín, Colombia`,
     movimiento: "comuna13",
-    shortText: "Emprendedor de Medellín",
-    text: `Empresario familiar, socio de las cremas más famosas de la comuna 13: lascremas de Doña Consuelo (su abuela), habitante del barrio San Javier.
+    shortText:
+      "Empresario familiar, socio de las cremas más famosas de la comuna 13",
+    text: `Empresario familiar, socio de las cremas más famosas de la comuna 13: las cremas de Doña Consuelo (su abuela), habitante del barrio San Javier.
 		Emprendedor por pasión y por vocación, lo lleva en la sangre, desde pequeñosiempre soñó con tener independencia económica y con ayudar a toda su
 		familia. Vio en el Graffitour de la Comuna 13 la oportunidad para sacaradelante un negocio familiar de helados.
 		Él se describe como un soñador, una persona que siempre ve oportunidades denegocio y lo más importante que las aprovecha. Ha logrado hacer del negociofamiliar una fuente de sustento y un referente para las demás familias delbarrio que como un efecto dominó empezaron a confiar en sus capacidadespara potenciar las ideas que tenían en mente, aprovechando el gran flujo deturistas que los visitan a diario.`,
