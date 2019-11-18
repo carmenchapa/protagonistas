97ab73ad10250f900a603c87740d8436a7809db3
Carmen Chapa <cmchapa28@gmail.com>
2019-11-18 01:49:51 +0100

    Updates

---
+++ precache-manifest.fa43fd82282f0fc1e86d380a72d3e75b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a694471e5c12a8b4c95478e384d9583",
+    "url": "/protagonistas/index.html"
+  },
+  {
+    "revision": "f7144e1c21447703184c",
+    "url": "/protagonistas/static/css/main.97ac244d.chunk.css"
+  },
+  {
+    "revision": "ea01713bc57cb74d2304",
+    "url": "/protagonistas/static/js/2.dae06904.chunk.js"
+  },
+  {
+    "revision": "f7144e1c21447703184c",
+    "url": "/protagonistas/static/js/main.fab82f87.chunk.js"
+  },
+  {
+    "revision": "41869adf315e725c1a84",
+    "url": "/protagonistas/static/js/runtime~main.d6de5b1b.js"
+  },
+  {
+    "revision": "26a1ec90be635d027479741f815494cb",
+    "url": "/protagonistas/static/media/Nunito-ExtraLight.26a1ec90.ttf"
+  },
+  {
+    "revision": "c41c2502180f63ce383b6e3cc042617a",
+    "url": "/protagonistas/static/media/Nunito-Light.c41c2502.ttf"
+  },
+  {
+    "revision": "cea08aabf611ab93a3ddc3925b5614a8",
+    "url": "/protagonistas/static/media/NunitoSans-Black.cea08aab.ttf"
+  },
+  {
+    "revision": "944f97c0f91efff6b678ac88304d2b77",
+    "url": "/protagonistas/static/media/aka1.944f97c0.png"
+  },
+  {
+    "revision": "40984f73433faded7b12dd3693ef388b",
+    "url": "/protagonistas/static/media/aka3.40984f73.jpg"
+  },
+  {
+    "revision": "56143f8ae869747e925bb0784094338b",
+    "url": "/protagonistas/static/media/art.56143f8a.svg"
+  },
+  {
+    "revision": "e73b40acd81a8f4b3f74efe9102c8fec",
+    "url": "/protagonistas/static/media/cremas.e73b40ac.jpg"
+  },
+  {
+    "revision": "174c011912c2606ad5e20854e5e5c53e",
+    "url": "/protagonistas/static/media/education.174c0119.svg"
+  },
+  {
+    "revision": "c6393d1b548ab1738399511863afbd84",
+    "url": "/protagonistas/static/media/ent.c6393d1b.svg"
+  },
+  {
+    "revision": "5cb88aa9675c4302df85f2270f8f5e52",
+    "url": "/protagonistas/static/media/homeBackground.5cb88aa9.jpg"
+  },
+  {
+    "revision": "6ac51102821b8eecc06ef84da22dd415",
+    "url": "/protagonistas/static/media/logo.6ac51102.svg"
+  },
+  {
+    "revision": "9c8eadb0b2e12dc6adbbe67bfbf329ac",
+    "url": "/protagonistas/static/media/map.9c8eadb0.png"
+  },
+  {
+    "revision": "707b273a8ba210abbdb8a1adc7d28c71",
+    "url": "/protagonistas/static/media/martika.707b273a.jpg"
+  },
+  {
+    "revision": "7b2c4cc57473a225415824db9cb4d486",
+    "url": "/protagonistas/static/media/perro.7b2c4cc5.jpg"
+  },
+  {
+    "revision": "2cd8888403bbc8ba4c94ffb4f5e07c77",
+    "url": "/protagonistas/static/media/science.2cd88884.svg"
+  },
+  {
+    "revision": "1a82bb9597b47322084a22db8351ea51",
+    "url": "/protagonistas/static/media/white_p.1a82bb95.svg"
+  }
+]);
